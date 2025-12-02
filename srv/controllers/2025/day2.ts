486c1776b9b6b0eb9b76505d2ff9c9eaf40f2777
Echono <wonger1324@gmail.com>
2025-12-02 12:40:20 +0100

    day2 pt2 done

--- srv/controllers/2025/day2.ts
+++ srv/controllers/2025/day2.ts
@@ -46,7 +46,39 @@ export class pt2Day2Controller {
     public static main: OnEventHandler = async (req: Request): Promise<void> => {
         
         const { input } = req.data;
+
+        const workload = ( input as string ).split(",").map( str => str.split("-"));
     
+        let result = 0;
+
+        workload.map((range) => {
+            return new Promise(() => {
+                const start = parseInt(range[0]);
+                const end = parseInt(range[1]);
+
+                for(let i = start; i <= end; i++) {
+
+                    if(i < 10) {
+                        continue;
+                    }
+
+                    const str = i.toString();
+
+                    for(let x = 0; x < Math.ceil(str.length / 2); x++) {
+                        const checkPart = str.substring(0, x + 1);
+                        const regexString: string = `.{1,${checkPart.length}}`;
+                        const chunksOfString = str.substring(x + 1).match(new RegExp(regexString, 'g'));
+                        const checks = chunksOfString!.every( chunk => chunk === checkPart );
+                        if(checks) {
+                            result += parseInt(str);
+                            break; 
+                        }
+                    }
+                }
+            })
+        });
+
+        req.reply(result);
 
     }
 }
