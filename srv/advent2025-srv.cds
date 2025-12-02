85228d9d3502b30e6517c47a214c81c84ef73b05
Echono <wonger1324@gmail.com>
2025-12-02 11:50:34 +0100

    refactoring

--- srv/advent2025-srv.cds
+++ srv/advent2025-srv.cds
@@ -1,6 +1,15 @@
 service advent2025 {
 
+    /* ######################### */
+    /* ######### DAY 1 ######### */
+    /* ######################### */
     action day1pt1(input: String) returns String;
     action day1pt2(input: String) returns String;
+
+    /* ######################### */
+    /* ######### DAY 2 ######### */
+    /* ######################### */
+    action day2pt1(input: String) returns String;
+    action day2pt2(input: String) returns String;
 
 }
--- srv/advent2025-srv.ts
+++ srv/advent2025-srv.ts
@@ -1,12 +1,22 @@
-import { day1pt1, day1pt2 } from "#cds-models/advent2025";
+import { day1pt1, day1pt2, day2pt1, day2pt2 } from "#cds-models/advent2025";
 import { ApplicationService } from "@sap/cds";
-import { pt1Day1Controller, pt2Day1Controller } from "./controllers/2025/day1Controller";
+import { pt1Day1Controller, pt2Day1Controller } from "./controllers/2025/day1";
+import { pt1Day2Controller, pt2Day2Controller } from "./controllers/2025/day2";
 
 export default class SalespriceService extends ApplicationService {
     async init(): Promise<void> {
 
+        /* ######################### */
+        /* ######### DAY 1 ######### */
+        /* ######################### */
         this.on(day1pt1, pt1Day1Controller.main);
         this.on(day1pt2, pt2Day1Controller.main);
+
+        /* ######################### */
+        /* ######### DAY 2 ######### */
+        /* ######################### */
+        this.on(day2pt1, pt1Day2Controller.main);
+        this.on(day2pt2, pt2Day2Controller.main);
 
     }
 }
