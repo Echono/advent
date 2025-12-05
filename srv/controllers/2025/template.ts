901facc0b6d1c30a5cd6ccda02140a2c6a9aee3a
Echono <wonger1324@gmail.com>
2025-12-05 14:27:24 +0100

    extended template

--- srv/controllers/2025/template.ts
+++ srv/controllers/2025/template.ts
@@ -1,5 +1,6 @@
 import { OnEventHandler, Request } from "@sap/cds";
 import { parseFileToString } from "../../util/parser";
+import { TrackPerformance } from "../../util/performer";
 
 /* ########################## */
 /* ######### PART 1 ######### */
@@ -7,13 +8,13 @@ import { parseFileToString } from "../../util/parser";
 
 export class pt1DayxyController {
     public static main: OnEventHandler = async (req: Request): Promise<void> => {
-        
+        const timer = new TrackPerformance(true, "Day xy, Part 1");
         const { fileName } = req.data;
-        const dir = __dirname + '/../../../inputs/2025/';
-        const input = parseFileToString(dir + fileName);
-        const workload = (input as string).split("\n");
+        const workload = parseData(fileName);
         let result = 0;
 
+        timer.end();
+        req.reply(result.toString());
     }
 }
 
@@ -23,12 +24,23 @@ export class pt1DayxyController {
 
 export class pt2DayxyController {
     public static main: OnEventHandler = async (req: Request): Promise<void> => {
-        
+        const timer = new TrackPerformance(true, "Day xy, Part 2");
         const { fileName } = req.data;
-        const dir = __dirname + '/../../../inputs/2025/';
-        const input = parseFileToString(dir + fileName);
-        const workload = (input as string).split("\n");
+        const workload = parseData(fileName);
         let result = 0;
 
+        timer.end();
+        req.reply(result.toString());
     }
+}
+
+/* ########################## */
+/* ######### PARSER ######### */
+/* ########################## */
+
+export function parseData(fileName: string): string[] {
+    const dir = __dirname + '/../../../inputs/2025/';
+    const input = parseFileToString(dir + fileName);
+    const workload = (input as string).split("\n");
+    return workload;
 }
